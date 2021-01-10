e028fb0a03c7869b7c33da58aa3e75246c49408a
afif ahmed <afifahmed456123@gmail.com>
2021-01-10 11:32:41 +0530

    changes in auth

--- auth/src/routes/__test__/current-user.test.ts
+++ auth/src/routes/__test__/current-user.test.ts
@@ -1,21 +1,21 @@
-import request from "supertest";
-import { app } from "../../app";
+import request from 'supertest';
+import { app } from '../../app';
 
-it("responds with details about the current user", async () => {
+it('responds with details about the current user', async () => {
   const cookie = await global.signin();
 
   const response = await request(app)
-    .get("/api/users/currentuser")
-    .set("Cookie", cookie)
+    .get('/api/users/currentuser')
+    .set('Cookie', cookie)
     .send()
-    .expect(200);
+    .expect(400);
 
-  expect(response.body.currentUser.email).toEqual("test@test.com");
+  expect(response.body.currentUser.email).toEqual('test@test.com');
 });
 
-it("responds with null if not authenticated", async () => {
+it('responds with null if not authenticated', async () => {
   const response = await request(app)
-    .get("/api/users/currentuser")
+    .get('/api/users/currentuser')
     .send()
     .expect(200);
 
